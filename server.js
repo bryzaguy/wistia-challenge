1f3236cbface39e3fcfbc693944751f43404f89c
Bryant Syme <prefu@hotmail.com>
2022-06-25 16:02:52 -0600

    add live reload

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const serveIndex = require('serve-index');
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
 
 const app = express();
 const port = 3000;
 
+app.use(connectLiveReload());
 app.use(serveIndex('client'));
 app.use(express.static('client'));
 
