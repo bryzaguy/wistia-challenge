c4d559004c9404e86c08025b78b043bba67603da
Bryant Syme <prefu@hotmail.com>
2022-06-30 02:43:20 -0700

    remove serve-index to fix api errors

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const serveIndex = require('serve-index');
 const livereload = require("livereload");
 const connectLiveReload = require("connect-livereload");
 
@@ -14,7 +13,6 @@ const app = express();
 const port = 3000;
 
 app.use(connectLiveReload());
-app.use(serveIndex('client'));
 app.use(express.static('client'));
 app.use(express.json())
 
